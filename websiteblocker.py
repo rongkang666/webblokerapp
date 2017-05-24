86fd89d0e2d2d07ba9ce5a9e58790c64cb18aa0b
Kang Rong <rongkang666@gmail.com>
2017-05-24 11:25:45 -0500

    first commit

---
+++ websiteblocker.py
@@ -0,0 +1,27 @@
+import time
+from datetime import datetime as dt
+
+hosts_path="/etc/hosts" #hosts file path
+redirect="127.0.0.1"
+website_list=["www.facebook.com","facebook.com"] #any website that you want to block
+
+while True:
+    if dt(dt.now().year,dt.now().month,dt.now().day,8) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,17):
+        print("Go to work...")
+        with open(hosts_path,'r+') as file:
+            content=file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect+" "+ website+"\n")
+    else:
+        with open(hosts_path,'r+') as file:
+            content=file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+        print("Have a rest...")
+    time.sleep(3)
